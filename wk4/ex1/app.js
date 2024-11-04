c0648da2b1854f24348af3a1f2dd250da15f987f
UciuIanis <uciuianis22@stud.ase.ro>
2024-11-04 15:37:00 +0200

    Added seminar 5

---
+++ wk4/ex1/app.js
@@ -0,0 +1,6 @@
+import {PI, circleArea} from './my-module.mjs'
+import { squareArea } from './my-common.cjs';
+
+console.log(circleArea(5));
+console.log(PI);
+console.log(squareArea(5));
---
+++ wk4/ex3/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+app.get('/hello', (req, res)=>{
+    res.send('Hello get')
+})
+app.post('/hello', (req, res)=>{
+    res.send('Hello post')
+})
+
+app.listen(8080)
